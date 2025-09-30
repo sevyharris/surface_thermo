3c5e7016066a08b1751d683da9c97908f1fbf421
Sevy Harris <harris.se@northeastern.edu>
2025-09-30 13:37:18 -0400

    add slurm run scripts for relaxation

--- system/run_Feoxide_calcs.sh
+++ system/run_Feoxide_calcs.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-#SBATCH --job-name=run_gemnet_cr2o3
+#SBATCH --job-name=run_gemnet_fe2o3
 #SBATCH --mem=20Gb
 #SBATCH --time=12:00:00
 #SBATCH --partition=short,west
-#SBATCH --export=ALL
 
 # define adsorbates
 adsorbates=("O" "H" "N" "C" "H2" "N2" "H2O" "O2" "NH3" "NH" "OH" "NO" "NH2" "CH3" "CH2" "CH" "CO" "CO2")
 
-for slabname in 'Cr2O3_z'; do
+for slabname in 'Fe2O3_z'; do
     for adsorbate in "${adsorbates[@]}"; do
-        for site in '0' '1' '2' '3' '4'; do
+        #for site in '0' '1' '2' '3' '4' '5' '6'; do
+        for site in '5' '6'; do
             for rotation in 0 90 180; do
                 echo "Running relaxation for $slabname + $adsorbate at site $site with rotation $rotation"
 
---
+++ system/run_Pt_fcc100.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH --job-name=run_gemnet_Pt_fcc100
+#SBATCH --mem=20Gb
+#SBATCH --time=12:00:00
+#SBATCH --partition=short,west
+#SBATCH --export=ALL
+
+
+# define adsorbates
+adsorbates=("O" "H" "N" "C" "H2" "N2" "H2O" "O2" "NH3" "NH" "OH" "NO" "NH2" "CH3" "CH2" "CH" "CO" "CO2")
+
+
+for metal in 'Pt' ; do
+   for facet in '100' ; do
+       for adsorbate in "${adsorbates[@]}"; do
+           for site in 'ontop' 'bridge' 'hollow' 'hcp'; do
+               for rotation in 0 90 180; do
+                   echo "Running relaxation for $metal $facet + $adsorbate at site $site with rotation $rotation"
+                   python relax_system.py --metal "$metal" --facet "$facet" --crystal_structure=fcc --adsorbate "$adsorbate" --site "$site" --rotate "$rotation"
+                   echo "Finished relaxation for $metal $facet + $adsorbate at site $site with rotation $rotation"
+               done
+           done
+       done
+   done
+done
+
+
